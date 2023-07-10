b1dc77eb8ced6da670b432e2787b94bd3ce71aa9
benzobak83 <vitanaolord@gmail.com>
2023-07-10 22:18:53 +0300

    feat: stylelint

--- src/shared/ui/LangSwitcher/LangSwitcher.tsx
+++ src/shared/ui/LangSwitcher/LangSwitcher.tsx
@@ -1,5 +1,6 @@
 import { Button } from '../Button/Button'
 import { FC } from 'react'
+import { cn } from 'shared/lib/classNames/classNames'
 import { useTranslation } from 'react-i18next'
 
 interface LangSwitcherProps {
@@ -12,5 +13,5 @@ export const LangSwitcher: FC<LangSwitcherProps> = ({ className }) => {
     const toggle = () => {
         i18n.changeLanguage(i18n.language === 'ru' ? 'en' : 'ru')
     }
-    return <Button onClick={toggle}>{t('Язык')}</Button>
+    return <Button className={cn('', {}, [className])} onClick={toggle}>{t('Язык')}</Button>
 }
