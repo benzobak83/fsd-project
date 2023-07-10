95ff1399b486794e22026e254f001259a4b57ff4
benzobak83 <vitanaolord@gmail.com>
2023-07-10 21:08:11 +0300

    feat: add babel, extract plugin

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -8,6 +8,27 @@ export function buildLoaders({ isDev }: BuildOptions): webpack.RuleSetRule[] {
     use: 'ts-loader',
     exclude: /node_modules/
   }
+
+  const babelLoader = {
+    test: /\.js|ts|tsx|jsx$/,
+    exclude: /node_modules/,
+    use: {
+      loader: 'babel-loader',
+      options: {
+        presets: ['@babel/preset-env'],
+        "plugins": [
+          [
+            'i18next-extract',
+            {
+              locales: ['ru', 'en'],
+              keyAsDefaultValue: true
+            }
+          ]
+        ]
+      }
+    }
+  }
+
   const cssLoader = {
     test: /\.s[ac]ss$/i,
     use: [
@@ -40,5 +61,5 @@ export function buildLoaders({ isDev }: BuildOptions): webpack.RuleSetRule[] {
     ]
   }
 
-  return [typescriptLoader, cssLoader, svgLoader, fileLoader]
+  return [babelLoader, typescriptLoader, cssLoader, svgLoader, fileLoader]
 }
